e6727837991eeb246a4674279f3ab6682890f8f5
HedCET <linto.cet@gmail.com>
2015-10-10 02:13:18 +0530

    delete for list item & patent item

--- client/linto/inbox.js
+++ client/linto/inbox.js
@@ -1,15 +1,15 @@
 Template.inbox.rendered = function() {
     patentability.find().observe({
         added: function(row) {
-            document.querySelector("layout-inbox").store(row);
+            document.querySelector("layout-inbox").bind(row);
         },
 
         changed: function(row, old) {
-            document.querySelector("layout-inbox").store(row);
+            document.querySelector("layout-inbox").bind(row);
         },
 
         removed: function(old) {
-            document.querySelector("layout-inbox")._store(old, true);
+            document.querySelector("layout-inbox").bind(old, true);
         }
     });
 };
